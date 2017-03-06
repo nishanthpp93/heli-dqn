8a2e5d6258d1daead007725be9fab5e71cf5d021
nishanthpp93 <nishanth.pp93@gmail.com>
2017-03-06 14:54:27 -0500

    Cleaned up a bit.

---
+++ Readme.md
@@ -0,0 +1,26 @@
+# Deep Q learning on Helicopter
+This project presents an effective way to play the popular Helicopter game using a convolutional neural network 
+to estimate the Q-function, using visual input of the game. It uses techniques described in [1] and improves
+upon it to play this specific game. 
+
+# Dependencies
+* Python 2.7
+* Pygame
+* Tensorflow
+* OpenCV-Python
+
+# References
+[1] Mnih Volodymyr, Koray Kavukcuoglu, David Silver, Andrei A. Rusu, Joel Veness, Marc G. Bellemare, Alex Graves, 
+Martin Riedmiller, Andreas K. Fidjeland, Georg Ostrovski, Stig Petersen, Charles Beattie, Amir Sadik, Ioannis 
+Antonoglou, Helen King, Dharshan Kumaran, Daan Wierstra, Shane Legg, and Demis Hassabis. Human-level Control 
+through Deep Reinforcement Learning. Nature, 529-33, 2015.
+
+[2] Volodymyr Mnih, Koray Kavukcuoglu, David Silver, Alex Graves, Ioannis Antonoglou, Daan Wierstra, and Martin 
+Riedmiller. Playing Atari with Deep Reinforcement Learning. NIPS, Deep Learning workshop
+
+# Disclaimer
+
+This work is highly based on the following repos:
+
+* [asrivat1/DeepLearningVideoGames](https://github.com/asrivat1/DeepLearningVideoGames)
+* [yenchenlin/DeepLearningFlappyBird](https://github.com/yenchenlin/DeepLearningFlappyBird)
--- wrapped_helicopter.py
+++ wrapped_helicopter.py
@@ -22,15 +22,15 @@ highscore = 0
 
 IMAGES = {}
 
-IMAGES['block'] = pygame.image.load('block2.png').convert_alpha()
+IMAGES['block'] = pygame.image.load('block.png').convert_alpha()
 
 IMAGES['heli'] = (
-    pygame.image.load('helicopter/1.1.1.png').convert_alpha(),
-    pygame.image.load('helicopter/2.1.1.png').convert_alpha(),
-    pygame.image.load('helicopter/3.1.1.png').convert_alpha()
+    pygame.image.load('helicopter/1.1.png').convert_alpha(),
+    pygame.image.load('helicopter/2.1.png').convert_alpha(),
+    pygame.image.load('helicopter/3.1.png').convert_alpha()
     )
 
-IMAGES['smoke'] = pygame.image.load('smoke2.png').convert_alpha()
+IMAGES['smoke'] = pygame.image.load('smoke.png').convert_alpha()
 
 IMAGES['wall'] = (
     pygame.image.load('wallup.png').convert_alpha(),
@@ -44,24 +44,24 @@ PLAYER_HEIGHT = IMAGES['heli'][0].get_height()
 class GameState:
     def __init__(self):
         self.score = self.playerIndex = self.loopIter = 0
-        self.playerx = 200
-        self.playery = 200
+        self.playerX = 200
+        self.playerY = 200
 
-        self.playeranim = [IMAGES['heli'][i] for i in range(3)]
-        self.playerim = self.playeranim[1]
-        self.playermask = pygame.mask.from_surface(self.playerim,50)
+        self.playerAnim = [IMAGES['heli'][i] for i in range(3)]
+        self.playerIM = self.playerAnim[1]
+        self.playerMask = pygame.mask.from_surface(self.playerIM,50)
         self.playerVel = SPEED
 
         newBlock = getRandomBlock(-100,300)
 
-        self.blockim = IMAGES['block']
-        self.blockmask = pygame.mask.from_surface(self.blockim,50)
+        self.blockIM = IMAGES['block']
+        self.blockMask = pygame.mask.from_surface(self.blockIM,50)
         self.blocks = [{'x': newBlock['x'], 'y': newBlock['y']}]
 
-        self.smokeim = IMAGES['smoke']
-        self.smokex = self.playerx
-        self.smokey = self.playery - 4
-        self.smokes = [{'x': self.smokex, 'y': self.smokey}]
+        self.smokeIM = IMAGES['smoke']
+        self.smokeX = self.playerX
+        self.smokeY = self.playerY - 4
+        self.smokes = [{'x': self.smokeX, 'y': self.smokeY}]
 
         self.blockVel = WALL_SPEED
         self.smokeVel = WALL_SPEED
@@ -69,19 +69,19 @@ class GameState:
         self.move = False
 
         #load wall images
-        self.walluim = IMAGES['wall'][0]
-        self.wallumask = pygame.mask.from_surface(self.walluim,50)
-        self.walldim = IMAGES['wall'][1]
-        self.walldmask = pygame.mask.from_surface(self.walldim,50)
+        self.walluIM = IMAGES['wall'][0]
+        self.walluMask = pygame.mask.from_surface(self.walluIM,50)
+        self.walldIM = IMAGES['wall'][1]
+        self.walldMask = pygame.mask.from_surface(self.walldIM,50)
 
-        self.wall_image_x = 0
-        self.wall_image_u_y = -100
-        self.wall_image_u_y = 400
+        self.wallIMX = 0
+        self.walluIMY = -100
+        self.walldIMY = 400
 
     def frame_step(self, input_actions):
         pygame.event.pump()
 
-        reward = 0.1
+        reward = 0.5
         terminal = False
 
         if sum(input_actions) != 1:
@@ -102,7 +102,7 @@ class GameState:
         #         move = False
 
         screen.fill(pygame.Color('black'))
-        screen.blit(self.playerim, (self.playerx, self.playery))
+        screen.blit(self.playerIM, (self.playerX, self.playerY))
 
         # player's movement
         if self.move:
@@ -110,7 +110,7 @@ class GameState:
                 self.playerVel -= 4
         elif self.playerVel < 6 and self.score%3 == 0:
             self.playerVel += 4
-        self.playery += self.playerVel
+        self.playerY += self.playerVel
 
         # playerIndex basex change
         if (self.loopIter + 1) % 3 == 0:
@@ -118,24 +118,24 @@ class GameState:
         self.loopIter = (self.loopIter + 1) % 30
 
         #displaying the wall:
-        self.wall_image_u_y = -100+self.score*0.05
-        self.wall_image_d_y = 400-self.score*0.05
+        self.walluIMY = -100+self.score*0.05
+        self.walldIMY = 400-self.score*0.05
 
-        if self.wall_image_x > -1400:
-            self.wall_image_x -= WALL_SPEED
+        if self.wallIMX > -1400:
+            self.wallIMX -= WALL_SPEED
         else :
-            self.wall_image_x += 1390
+            self.wallIMX += 1390
 
-        screen.blit(IMAGES['wall'][0],(self.wall_image_x,self.wall_image_u_y))
-        screen.blit(IMAGES['wall'][1],(self.wall_image_x,self.wall_image_d_y))
+        screen.blit(IMAGES['wall'][0],(self.wallIMX,self.walluIMY))
+        screen.blit(IMAGES['wall'][1],(self.wallIMX,self.walldIMY))
 
-        #blocks:
+        #block:
         for block in self.blocks:
             block['x'] -= self.blockVel
             screen.blit(IMAGES['block'],(block['x'],block['y']))
 
         if 45 < self.blocks[0]['x'] < 55:
-            newBlock = getRandomBlock(self.wall_image_u_y, self.wall_image_d_y)
+            newBlock = getRandomBlock(self.walluIMY, self.walldIMY)
             self.blocks.append(newBlock)
 
         if 145 < self.blocks[0]['x'] < 155: 
@@ -149,60 +149,45 @@ class GameState:
             smoke['x'] -= self.smokeVel
             screen.blit(IMAGES['smoke'],(smoke['x'] - random.randint(0,15),smoke['y']))
 
-        if self.smokes[0]['x'] < self.playerx - 15:
-            self.smokes.append({'x': self.playerx, 'y': self.playery - 4})
+        if self.smokes[0]['x'] < self.playerX - 15:
+            self.smokes.append({'x': self.playerX, 'y': self.playerY - 4})
 
         if self.smokes[0]['x'] < -15:
             self.smokes.pop(0)
 
         screen.blit(small_font.render('SCORE: %i'%self.score,False,pygame.Color(0,100,255)),(50,460))
 
-        isCrash = check_collide(self.playerx,self.playery,self.wall_image_x,self.wall_image_u_y,self.wall_image_d_y,
-            self.wallumask,self.walldmask,self.playermask,self.blockmask,self.blocks)
-
-        #isCrash = check_collide(self.playerx,self.playery,self.playermask,self.blockmask,self.blocks)
+        isCrash = check_collide(self.playerX,self.playerY,self.wallIMX,self.walluIMY,self.walldIMY,
+            self.walluMask,self.walldMask,self.playerMask,self.blockMask,self.blocks)
 
         if isCrash:
             terminal = True
             self.__init__()
             reward = -1
 
-        # if check_collide(helicopter,wall_image_x,wall_prime_x, prime_mask, wall_mask, helicopter_mask, blocks):
-        #     pygame.draw.circle(screen, pygame.Color('red'), (helicopter.x+39,helicopter.y+18), 60, 3)
-        #     pygame.display.update()
-        #     while not restart:
-        #         for event in pygame.event.get():
-        #             if event.type == QUIT:
-        #                 pygame.quit()
-        #                 sys.exit()
-        #             if event.type == MOUSEBUTTONDOWN:
-        #                 restart = True
-
-
-        self.playerim = self.playeranim[self.score%3]
+        self.playerIM = self.playerAnim[self.score%3]
 
         image_data = pygame.surfarray.array3d(pygame.display.get_surface())
-        crash_img = image_data[self.playerx:self.playerx+PLAYER_WIDTH+PLAYER_WIDTH/4, self.playery-PLAYER_HEIGHT:self.playery+PLAYER_HEIGHT*2].copy()
-        crash_img[0:PLAYER_WIDTH, PLAYER_HEIGHT-10:PLAYER_HEIGHT*2+10] = (0,0,0)
+        crashIM = image_data[self.playerX:self.playerX+PLAYER_WIDTH+PLAYER_WIDTH/4, self.playerY-PLAYER_HEIGHT:self.playerY+PLAYER_HEIGHT*2].copy()
+        crashIM[0:PLAYER_WIDTH, PLAYER_HEIGHT-10:PLAYER_HEIGHT*2+10] = (0,0,0)
         pygame.display.update()
         clock.tick(FPS)
         self.score += 1
-        return image_data[self.playerx:,:].copy(), reward, terminal, crash_img
+        return image_data[self.playerX:,:].copy(), reward, terminal, crashIM
     
 
-def check_collide(x, y, wall_image_x, wall_image_u_y, wall_image_d_y, wallumask, walldmask, playermask, blockmask, blocks):
+def check_collide(x, y, wallIMX, walluIMY, walldIMY, walluMask, walldMask, playerMask, blockMask, blocks):
     """uses masks to see if non-transparent pixels overlap"""
     #check if the wall has collided:
-    wall_offset_x = x - wall_image_x
-    wall_u_offset_y = y - int(wall_image_u_y)
-    wall_d_offset_y = y - int(wall_image_d_y)
-    offset_y = y
-    if wallumask.overlap(playermask,(wall_offset_x,wall_u_offset_y)) or walldmask.overlap(playermask,(wall_offset_x,wall_d_offset_y)):
+    wallOffsetX = x - wallIMX
+    walluOffsetY = y - int(walluIMY)
+    walldOffsetY = y - int(walldIMY)
+    if walluMask.overlap(playerMask,(wallOffsetX,walluOffsetY)) or walldMask.overlap(playerMask,(wallOffsetX,walldOffsetY)):
         return True
     #check if the blocks have collided:
-    offset_x = x - blocks[0]['x']
-    offset_y = y - blocks[0]['y']
-    overlap = blockmask.overlap(playermask,(offset_x,offset_y))
+    offsetX = x - blocks[0]['x']
+    offsetY = y - blocks[0]['y']
+    overlap = blockMask.overlap(playerMask,(offsetX,offsetY))
     if overlap: return True
 
 # def check_collide(x,y,playermask, blockmask, blocks):
@@ -217,7 +202,7 @@ def check_collide(x, y, wall_image_x, wall_image_u_y, wall_image_d_y, wallumask,
 #         overlap = blockmask.overlap(playermask,(offset_x,offset_y))
 #         if overlap: return True
 
-def getRandomBlock(wall_image_u_y, wall_image_d_y):
-    blockx = SCREEN_WIDTH #right out of view
-    blocky = random.randint(int(wall_image_u_y) + 200, int(wall_image_d_y) - 100)
-    return {'x': blockx, 'y': blocky}
+def getRandomBlock(walluIMY, walldIMY):
+    blockX = SCREEN_WIDTH #right out of view
+    blockY = random.randint(int(walluIMY) + 200, int(walldIMY) - 100)
+    return {'x': blockX, 'y': blockY}
